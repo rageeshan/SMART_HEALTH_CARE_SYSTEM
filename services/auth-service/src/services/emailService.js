ac519578db13ec295d67d10a11379f7b5b1671db
Suprajan Jeyapal <jsuprajan@gmail.com>
2026-04-16 06:57:32 +0530

    done with auth-service

--- services/auth-service/src/services/emailService.js
+++ services/auth-service/src/services/emailService.js
@@ -1,4 +1,10 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
